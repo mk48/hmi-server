2b625030ef60cab561ec17f8956745a9b69bc99a
M Kumaran <veerammalkumaran@gmail.com>
2021-10-26 20:15:37 +0530

    data changed as array

--- routes/data.js
+++ routes/data.js
@@ -1,16 +1,27 @@
 var express = require("express");
 var router = express.Router();
 
-let dt = { tag1: 20, tag2: 5, tag3: 200, tagcolor1: "red", tagcolor2: "blue" };
+let PLCdata = [
+  { name: "motor1.temperature", type: "analog", val: 80, unit: "F" },
+  { name: "motor1.speed", type: "analog", val: 20, unit: "RPM" },
+  { name: "motor1.state", type: "digital", val: 1 },
+  { name: "belt1.speed", type: "analog", val: 0.5, unit: "m/sec" },
+  { name: "belt1.state", type: "digital", val: 0 },
+];
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
-  res.json(dt);
+  res.json(PLCdata);
 });
 
 router.post("/", function (req, res) {
-  dt = { ...dt, ...req.body };
-  res.json({ result: true });
+  const updateRequest = req.body;
+  const tagValueToUpdate = PLCdata.find((d) => d.name === updateRequest.name);
+  if (tagValueToUpdate && tagValueToUpdate.name) {
+    tagValueToUpdate.val = updateRequest.val;
+    res.json({ result: true });
+  } else {
+    res.json({ result: false });
+  }
 });
 
 module.exports = router;
