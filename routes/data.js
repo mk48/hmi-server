247d4c2ebac8e643842e1aac11ea9f0399031874
M Kumaran <veerammalkumaran@gmail.com>
2021-11-10 15:53:55 +0530

    tag values saved in json file

--- routes/data.js
+++ routes/data.js
@@ -1,13 +1,13 @@
 var express = require("express");
+const fs = require("fs");
+const path = require("path");
 var router = express.Router();
 
-let PLCdata = [
-  { name: "motor1.temperature", type: "analog", val: 80, unit: "F" },
-  { name: "motor1.speed", type: "analog", val: 20, unit: "RPM" },
-  { name: "motor1.state", type: "digital", val: 1 },
-  { name: "belt1.speed", type: "analog", val: 0.5, unit: "m/sec" },
-  { name: "belt1.state", type: "digital", val: 0 },
-];
+let PLCdata = [];
+
+const filePath = path.join(__dirname, "../data/plcdata.json");
+
+loadData();
 
 router.get("/", function (req, res, next) {
   res.json(PLCdata);
@@ -18,10 +18,30 @@ router.post("/", function (req, res) {
   const tagValueToUpdate = PLCdata.find((d) => d.name === updateRequest.name);
   if (tagValueToUpdate && tagValueToUpdate.name) {
     tagValueToUpdate.val = updateRequest.val;
-    res.json({ result: true });
   } else {
-    res.json({ result: false });
+    //if tag not found, add it
+    PLCdata.push(updateRequest);
   }
+  saveData();
+  res.json({ result: true });
 });
+
+function saveData() {
+  const data = JSON.stringify(PLCdata);
+  try {
+    fs.writeFileSync(filePath, data, "utf8");
+  } catch (exp) {
+    console.error(exp);
+  }
+}
+
+function loadData() {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    PLCdata = JSON.parse(data);
+  } catch (exp) {
+    console.error(exp);
+  }
+}
 
 module.exports = router;
